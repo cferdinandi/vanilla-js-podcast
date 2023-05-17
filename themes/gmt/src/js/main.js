aeb40ddc38feeb6064dee1c6acb1e21bf025daa0
cferdinandi <chris@gomakethings.com>
2023-05-17 11:25:51 -0400

    New episode

--- themes/gmt/src/js/main.js
+++ themes/gmt/src/js/main.js
@@ -1,4 +1,9 @@
 import convertkit from '../../../../../gmt-theme/dist/js/convertkit.js';
+import {products, getAffiliate, setAffiliate} from '../../../../../gmt-theme/dist/js/affiliates.js';
 
 // ConvertKit form
 convertkit();
+
+// Affiliate marketing
+getAffiliate();
+setAffiliate(products);
